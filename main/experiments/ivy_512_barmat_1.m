04bb290c0874c218a8add326c618c37071ea4b28
Erik Nijkamp <erik.nijkamp@gmail.com>
2017-09-15 13:01:00 -0700

    timing

--- main/experiments/ivy_512_barmat_1.m
+++ main/experiments/ivy_512_barmat_1.m
@@ -1,6 +1,6 @@
 function [] = ivy_512_barmat_1()
 
-exp_time = tic;
+tot_time = tic;
 
 % matconvnet
 addpath(genpath('../../matconvnet-1.0-beta16/'));
@@ -20,6 +20,9 @@ file_str = 'ivy/512/';
 config = gen_ADELM_config(file_str);
 
 for i = 1:10
+    disp(['exp ' num2str(i)]);
+    exp_time = tic;
+
     %load complete ELM experiment
     load([config.ELM_folder,file_str,'ELM_exp',...
             num2str(i),'.mat']);
@@ -36,14 +39,19 @@ for i = 1:10
     %save results
     if ~exist([ELM.config.tree_folder,file_str],'dir') mkdir([ELM.config.tree_folder,file_str]); end
     save([ELM.config.tree_folder,file_str,'/bar_mat',num2str(i),'.mat'],'bar_mat');
+    
+    % time
+    exp_time = toc(exp_time);
+    fprintf('Experiment Time: %4d hours %4.2f minutes \n',...
+                         floor(exp_time/3600), mod(exp_time/60,60));
 end
 
 % parpool end
 delete(pool);
 
-exp_time = toc(exp_time);
+tot_time = toc(tot_time);
 fprintf('Total Experiment Time: %4d hours %4.2f minutes \n',...
-                     floor(exp_time/3600), mod(exp_time/60,60));
+                     floor(tot_time/3600), mod(tot_time/60,60));
         
 
 end
